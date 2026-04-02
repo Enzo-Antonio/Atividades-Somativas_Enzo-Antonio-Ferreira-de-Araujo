7fc8013a1fc64b304b2d6973fff275f85212bb5d
Enzo Antônio Ferreira de Araújo <enzo.a.araujo6@aluno.senai.br>
2026-04-02 15:29:52 -0300

    Avaliação Somativa - 02/04/2026
    Realização da Avaliação Somativa.

--- FRONT-END/src/scripts/script.js
+++ FRONT-END/src/scripts/script.js
@@ -1,2 +1,53 @@
-const dataHora = document.querySelector('#saudacao-voluntario')
-dataHora
+// Missão 1
+const horario = document.querySelector('#saudacao-voluntario')
+const timeIs = new Date()
+const hora = timeIs.getHours()
+ if (hora < 12) {
+    alert('Plantão matinal, Protetor!')
+ }
+ if (hora >= 12 && hora < 18) {
+    alert('Plantão da tarde, Protetor!')
+ }
+ if (hora <= 18 && hora < 22) {
+    alert('Plantão noturno, Protetor!')
+ }
+
+ // Missão 2
+const bannerMouse = document.querySelector('#banner-adocao')
+bannerMouse.addEventListener('mouseover', () => {
+    bannerMouse.classList.toggle('destaque-pet')
+})
+bannerMouse.addEventListener('mouseout', () => {
+    bannerMouse.classList.toggle('destaque-pet')
+})
+
+// Missão 3
+const calculo = document.querySelector('#idade-pet')
+const total = document.querySelector('#idade-humana')
+
+if (calculo != "") {
+
+}
+
+total.textContent = calcular(calculo)
+
+// Missão 4
+const cadastroBtn = document.querySelector('#btn-cadastrar')
+const nomePet = document.querySelector('#nome-pet')
+const listaAdocao = document.querySelector('#lista-adocao')
+
+nomePet.addEventListener('input', () => {
+    const nome = nomePet.value
+})
+
+cadastroBtn.addEventListener('click', () => {
+    const novoCadastro =  <article class="card-adocao"><h3>nome</h3></article>;
+    listaAdocao.innerHTML += novoCadastro
+})
+
+// Missão 5
+const limpaBtn = document.querySelector('#btn-limpar')
+
+limpaBtn.addEventListener('click', () => {
+    listaAdocao.innerHTML = ""
+})
